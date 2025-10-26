3f093f995516e2ea3cb15b601a5a0e86cd69ca91
programmers-porch <varadlawate73@gmail.com>
2025-10-26 16:24:55 +0530

    Delete db directory

--- db/views.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW top_movies AS
-SELECT m.title, m.release_year, d.director_name, m.imdb_rating
-FROM movies m
-JOIN directors d ON m.director_id = d.director_id
-WHERE m.imdb_rating >= 8.5
-ORDER BY m.imdb_rating DESC;
